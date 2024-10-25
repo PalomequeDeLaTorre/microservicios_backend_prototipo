d5787a8f79ed06655c6e8181e9cea40f6b5a56f9
PalomequeDeLaTorre <carlospd.ti22@utsjr.edu.mx>
2024-10-25 13:13:12 -0600

    microservicios

--- api_gateway/public/script.js
+++ api_gateway/public/script.js
@@ -3,18 +3,19 @@ function redirigirA(url) {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('a[href="#eventos"]').addEventListener('click', (e) => {
-        e.preventDefault(); 
+    const eventoBtn = document.querySelector('.evento-btn');
+    const compraBtn = document.querySelector('.compra-btn');
+    const confirmacionBtn = document.querySelector('.confirmacion-btn');
+
+    eventoBtn.addEventListener('click', () => {
         redirigirA('https://eventos-microservicio-1.onrender.com');
     });
 
-    document.querySelector('a[href="#compras"]').addEventListener('click', (e) => {
-        e.preventDefault();
+    compraBtn.addEventListener('click', () => {
         redirigirA('https://compra-microservicio.onrender.com');
     });
 
-    document.querySelector('a[href="#confirmaciones"]').addEventListener('click', (e) => {
-        e.preventDefault();
+    confirmacionBtn.addEventListener('click', () => {
         redirigirA('https://confirmacion-microservicio.onrender.com');
     });
 });
